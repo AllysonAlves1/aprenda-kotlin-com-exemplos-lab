56034b379c197de820783a0292bdeb90c354640d
Allyson <allysonalves.01@gmail.com>
2024-02-01 10:52:18 -0300

    Conclusão dos TODOs

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,47 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, val idade: Int) {
+    override fun toString(): String {
+        return "$nome"
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel = Nivel.BASICO)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+//        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário $usuario matriculado na formação $nome")
+    }
+    fun alunosMatriculados() {
+        println("Alunos matriculados na formação $nome:")
+        inscritos.forEach { println(it) }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+//    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+//    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val formacao = Formacao("Kotlin para iniciantes", listOf(
+        ConteudoEducacional("O que é Kotlin?", 30, Nivel.BASICO),
+        ConteudoEducacional("Primeiros passos com Kotlin", 60, Nivel.INTERMEDIARIO),
+        ConteudoEducacional("Kotlin avançado", 90, Nivel.DIFICIL),
+    ))
+
+    val usuario = Usuario("João", 25)
+    val usuario2 = Usuario("Maria", 30)
+    val usuario3 = Usuario("José", 40)
+
+    formacao.matricular(usuario)
+    formacao.matricular(usuario2)
+    formacao.matricular(usuario3)
+
+    formacao.alunosMatriculados()
+
 }
